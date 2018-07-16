be508300a6deab202daf346de894f4488e7a2c58
Egor Egorov <erorrov@gmail.com>
2018-07-17 00:56:34 +0300

    ex1

---
+++ ex1.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var a: Int8
+var b: UInt8
+
+a = Int8.max
+b = UInt8.max
+
+print(a, b)
+
+var c = 9
+var d: Int = 3
+
+var tmp = c
+c = d
+d = tmp
+
+print("c =", c, "\nd =", d)
