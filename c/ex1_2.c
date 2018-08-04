4a74cfc4d00c83aabd6a81a53989728e94526de9
Egor Egorov <erorrov@gmail.com>
2018-08-04 19:00:50 +0300

    C lang

---
+++ c/ex1_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main() {
+    printf("\nhello\n");
+    printf("\rhello\r");
+    printf("\thello\t");
+    printf("\ahello\a");
+    printf("\bhello\b");
+    printf("\vhello\v");
+    printf("\fhello\f");
+    printf("\rhello\r");
+}
