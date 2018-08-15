008c42496010c63c6959a865a1e40e0416a088dd
Egor Egorov <erorrov@gmail.com>
2018-08-15 13:58:04 +0300

    1.17

---
+++ c/ex1_17.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+#define LIMIT 80
+
+int readline(char s[], int maxline);
+
+int main() {
+    char line[MAXLINE];
+    int len;
+
+    while ((len = readline(line, MAXLINE)) > 0) {
+        if (len > LIMIT) {
+            printf("%s", line);
+        }
+    }
+}
+
+int readline(char s[], int maxline) {
+    int c, i;
+
+    for (i = 0; i < maxline-1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+
+    return i;
+}
