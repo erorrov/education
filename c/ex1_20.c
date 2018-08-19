adfb97d58acfbe4d7302e42c9dfb5f51b65e6e6b
Egor Egorov <erorrov@gmail.com>
2018-08-20 01:45:33 +0300

    1.20

---
+++ c/ex1_20.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define TABSIZE 8
+
+int main() {
+    int c;
+    int column = TABSIZE;
+
+    while ((c = getchar()) != EOF) {
+        if (c != '\t') {
+            column = column > 0 ? --column : TABSIZE-1;
+            putchar(c);
+        } else {
+            for (int i = column; column > 0; --column) {
+                putchar(' ');
+            }
+
+        }
+    }
+}
