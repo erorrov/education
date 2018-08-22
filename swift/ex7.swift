d514ee36edba788964551b54a443d6d16a80fce5
Egor Egorov <erorrov@gmail.com>
2018-08-23 01:57:52 +0300

    swift 7 & 8

---
+++ swift/ex7.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+var randomStr: String = "hello"
+let randomChar: Character = "w"
+
+var randomA: Int = 2308
+var randomB: Int = 2018
+
+let trash = randomStr + String(randomChar) + String(randomA + randomB)
+
+print(trash)
---
+++ swift/ex8.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+//so stupid exercises...
+
+//*   *   *
+// * * * *
+//  *   *
+
+print("*   *   *")
+print(" * * * *")
+print("  *   *")
