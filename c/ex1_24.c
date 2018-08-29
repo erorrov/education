7f5a6355b0be0508b67cc5b9a187fff408357584
Egor Egorov <erorrov@gmail.com>
2018-08-29 21:06:53 +0300

    fix 1.24

--- c/ex1_24.c
+++ c/ex1_24.c
@@ -1,112 +1,146 @@
 #include <stdio.h>
 
+#define MAX_ERR_COUNT 1000
+
 #define OUT 0
 #define IN 1
 
-/*
- * The code looks awful
- * I'll fix it later...
- */
+#define EXPECTED_EXPRESSION_PAREN 0
+#define EXPECTED_EXPRESSION_BRACE 1
+#define EXPECTED_EXPRESSION_BRACKET 2
+#define SINGLE_QUOTE 3
+#define DOUBLE_QUOTE 4
+#define PAREN_OPEN 5
+#define BRACE_OPEN 6
+#define BRACKET_OPEN 7
 
-int main() {
-    //current line count
-    int line = 0;
+int err_counter = 0;
+int err_lines[MAX_ERR_COUNT];
+int err_codes[MAX_ERR_COUNT];
+
+int paren = 0;      // ()
+int brace = 0;      // {}
+int bracket = 0;    // []
+
+int paren_open_line = -1;
+int brace_open_line = -1;
+int bracket_open_line = -1;
+
+int squote = OUT;   // '
+int dquote = OUT;   // "
+int quote_open_line = -1;
+
+void add_err(int line, int code) {
+    if (err_counter >= MAX_ERR_COUNT)
+        return;
     
-    //states
-    int paren = 0;
-    int brace = 0;
-    int bracket = 0;
-    int charLiteral = OUT;
-    int strMark = OUT;
+    if (err_lines[err_counter-1] == line && err_codes[err_counter-1] == code)
+        return;
     
-    //lines with errors
-    int parenLineFirst = -1;
-    int braceLineFirst = -1;
-    int bracketLineFirst = -1;
-    int charLiteralLine = -1;
-    int strMarkLine = -1;
-    
-    int parenLineLast = -1;
-    int braceLineLast = -1;
-    int bracketLineLast = -1;
-    
+    err_lines[err_counter] = line;
+    err_codes[err_counter] = code;
+    ++err_counter;
+}
+
+void read_code(void) {
     int c;
-    
+    int line = 0;
     while ((c = getchar()) != '1') {
-        //TODO: escape
-        if (charLiteral == IN && c != '\'') { continue; }
-        if (strMark == IN && c != '"') { continue; }
+        if (c != '\'' && squote == IN) { continue; }
+        if (c != '"' && dquote == IN) { continue; }
         
         switch (c) {
-            case '\n':
-                ++line;
-                break;
-                
             case '(':
-                parenLineLast = line;
+                if (paren_open_line == -1) paren_open_line = line;
                 ++paren;
                 break;
                 
-            case ')':
-                if (paren <= 0 && parenLineFirst == -1) { parenLineFirst = line; }
-                --paren;
-                break;
-                
             case '{':
-                braceLineLast = line;
+                if (brace_open_line == -1) brace_open_line = line;
                 ++brace;
                 break;
                 
-            case '}':
-                if (brace <= 0 && braceLineFirst == -1) { braceLineFirst = line; }
-                --brace;
-                break;
-                
             case '[':
-                bracketLineLast = line;
+                if (bracket_open_line == -1) bracket_open_line = line;
                 ++bracket;
                 break;
                 
+            case ')':
+                if (paren == 1) { paren_open_line = -1; }
+                else if (paren <= 0) { add_err(line, EXPECTED_EXPRESSION_PAREN); }
+                --paren;
+                break;
+                
+            case '}':
+                if (brace == 1) { brace_open_line = -1; }
+                else if (brace <= 0) { add_err(line, EXPECTED_EXPRESSION_BRACE); }
+                --brace;
+                break;
+                
             case ']':
-                if (bracket <= 0 && bracketLineFirst == -1) { bracketLineFirst = line; }
+                if (bracket == 1) { bracket_open_line = -1; }
+                else if (bracket <= 0) { add_err(line, EXPECTED_EXPRESSION_BRACKET); }
                 --bracket;
                 break;
                 
             case '\'':
-                charLiteral = charLiteral == OUT ? IN : OUT;
-                charLiteralLine = line;
+                squote = squote == OUT ? IN : OUT;
+                quote_open_line = line;
                 break;
                 
-            case '"':
-                strMark = strMark == OUT ? IN : OUT;
-                strMarkLine = line;
+            case '\"':
+                dquote = dquote == OUT ? IN : OUT;
+                quote_open_line = line;
+                break;
+                
+            case '\n':
+                ++line;
                 break;
                 
             default:
                 break;
         }
+        
     }
+}
+
+void check_errs(void) {
+    if (squote == IN)
+        add_err(quote_open_line, SINGLE_QUOTE);
     
+    if (dquote == IN)
+        add_err(quote_open_line, DOUBLE_QUOTE);
     
-    if (parenLineFirst != -1)
-        printf("ERROR: unbalanced parenthesis on line %d\n", parenLineFirst+1);
-    if (braceLineFirst != -1)
-        printf("ERROR: unbalanced brace on line %d\n", braceLineFirst+1);
-    if (bracketLineFirst != -1)
-        printf("ERROR: unbalanced bracket on line %d\n", bracketLineFirst+1);
+    if (paren != 0 && paren_open_line != -1)
+        add_err(paren_open_line, PAREN_OPEN);
     
-    if (charLiteral != OUT)
-        printf("ERROR: single quote symbol is not closed on line %d\n", charLiteralLine+1);
-    if (strMark != OUT)
-        printf("ERROR: double quote symbol is not closed on line %d\n", strMarkLine+1);
+    if (brace != 0 && brace_open_line != -1)
+        add_err(brace_open_line, BRACE_OPEN);
     
-    if (paren != 0 && parenLineFirst == -1)
-        printf("ERROR: parenthesis is not closed on line %d\n", parenLineLast+1);
-    if (brace != 0 && braceLineFirst == -1)
-        printf("ERROR: brace is not closed on line %d\n", braceLineLast+1);
-    if (bracket != 0 && bracketLineFirst == -1)
-        printf("ERROR: bracket is not closed on line %d\n", bracketLineLast+1);
+    if (bracket != 0 && bracket_open_line != -1)
+        add_err(bracket_open_line, BRACKET_OPEN);
+}
+
+void print_errs(void) {
+    const char *desc[8];
+    desc[0] = "unbalanced parenthesis";
+    desc[1] = "unbalanced brace";
+    desc[2] = "unbalanced bracket";
+    desc[3] = "single quote symbol is not closed";
+    desc[4] = "double quote symbol is not closed";
+    desc[5] = "parenthesis is not closed";
+    desc[6] = "brace is not closed";
+    desc[7] = "bracket is not closed";
     
-    //TODO: fix it
-    return 0;
+    for (int i = 0; i < err_counter; ++i) {
+        printf("ERROR: %s on line %d\n", desc[err_codes[i]], err_lines[i]+1);
+    }
+}
+
+int main() {
+    read_code();
+    check_errs();
+    print_errs();
+    
+    return err_counter;
 }
