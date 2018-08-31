93c00c158ba645176d507dd66c93573c36502995
Egor Egorov <erorrov@gmail.com>
2018-08-31 16:03:04 +0300

    2.1 fixes

--- c/ex2_1.c
+++ c/ex2_1.c
@@ -9,6 +9,24 @@ int main () {
     printf("%-15s %-20d %-20d\n", "unsigned char", 0, UCHAR_MAX);
     printf("%-15s %-20d %-20d\n", "signed char", SCHAR_MIN, SCHAR_MAX);
     printf("%-15s %-20d %-20d\n", "signed short", SHRT_MIN, SHRT_MAX);
-    printf("%-15s %-20d %-20d\n", "int", INT_MIN, INT_MAX);
-    printf("%-15s %-20li %-20li\n", "signed long", LONG_MIN, LONG_MIN);
+    printf("%-15s %-20d %-20d\n", "signed int", INT_MIN, INT_MAX);
+    printf("%-15s %-20li %-20li\n", "signed long", LONG_MIN, LONG_MAX);
+    
+    printf("\nComputing...\n"); //char, short, int, long, double, float
+    
+    char ch = 0;
+    while (++ch > 0);
+    short sh = 0;
+    while (++sh > 0);
+    int in = 0;
+    while (++in > 0);
+    
+    printf("%-15s %-20d %-20u\n", "unsigned int", 0, in * -2 - 1);
+    printf("%-15s %-20d %-20d\n", "unsigned short", 0,  sh * -2 - 1);
+    printf("%-15s %-20d %-20d\n", "unsigned char", 0,  ch * -2 - 1);
+    printf("%-15s %-20d %-20d\n", "signed char", ch,  -ch - 1);
+    printf("%-15s %-20d %-20d\n", "signed short", sh,  -sh - 1);
+    printf("%-15s %-20d %-20d\n", "signed int", in,  -in - 1);
+    
+    //TODO: float, double, long
 }
